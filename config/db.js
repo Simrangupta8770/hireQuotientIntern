3d6b7688f18ca725e2e18096114b0c4a59da2569
Simrangupta8770 <84565804+Simrangupta8770@users.noreply.github.com>
2023-12-02 04:57:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://simrangupta9630:EohKCKuBvUNawcSp@cluster0.wte66wq.mongodb.net/?retryWrites=true&w=majority', {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
